33302321af41cd2826b4a446eeef6613aa35309d
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-09-05 08:48:56 -0300

    Começo da criação das rotas

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const mysql = require("mysql");
+const path = require("path");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/entrar", (req, res) => {
+    res.render("entrar");
+});
+
+app.get("/cadastrar", (req, res) => {
+    res.render("cadastrar");
+});
+
+app.get("/opcaoCadastro", (req, res) => {
+    res.render("opcaoCadastro");
+});
+
+app.get("/sobre", (req, res) => {
+    res.render("sobre");
+});
+
+app.get("abrirChamado", (req, res) => {
+    res.render("abrirChamado");
+});
+
+app.listen(80);
