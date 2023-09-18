4c0652251a9fc53388040b7b4d95f8359e14fdaa
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-09-18 19:24:50 -0300

    Integração front e back para página de abrir e ver chamados

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mysql = require("mysql");
 const path = require("path");
 const app = express();
+const Chamado = require("./models/chamado");
+
+let chamados = [];
 
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs")
@@ -30,8 +33,29 @@ app.get("/abrirChamado", (req, res) => {
     res.render("abrirChamado");
 });
 
+app.post("/abrirChamado", (req, res) => {
+    let title = req.body.titulo;
+    let desc = req.body.desc;
+    let comentario = req.body.comentario;
+    let nomeEquipamento = "";
+    let numeroSerie = "";
+    let tipoEquipamento = "";
+    try {
+        nomeEquipamento = req.body.equipamentonome;
+    } catch (error) {};
+    try {
+        numeroSerie = req.body.numeroserie;
+    } catch (error) {};
+    try {
+        tipoEquipamento = req.body.equipamentotipo;
+    } catch (error) {};
+    let novoChamado = new Chamado(title, desc, comentario, nomeEquipamento, numeroSerie, tipoEquipamento);
+    chamados.push(novoChamado);
+    res.send("Chamado aberto com sucesso!<br><a href='/chamados'>Ver Chamados</a>");
+});
+
 app.get("/chamados", (req, res) => {
-    res.render("chamados");
+    res.render("chamados", {chamados: chamados});
 });
 
 app.get("/chamadoAberto", (req, res) => {
