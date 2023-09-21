0188ec00542145239732f47fbda6262bba03a08d
Wellington Luiz de Faria <91472871+WellingtonLFaria@users.noreply.github.com>
2023-09-21 08:52:31 -0300

    Começo do desenvolvimento com React
    O banco de dados já está integrado no sistema

---
+++ call-genie/src/Chamados.tsx
@@ -0,0 +1,9 @@
+const db = require();
+
+function Chamados() {
+    return (
+        <>
+        item
+        </>
+    );
+};
---
+++ call-genie/src/bd.tsx
@@ -0,0 +1,225 @@
+import {Sequelize, DataTypes} from "sequelize"
+
+const database = new Sequelize('callgenie', 'root', 'fatec', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+const Cliente = database.define('Cliente', {
+    cli_nome: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    cli_cpf: {
+        type: DataTypes.STRING(11),
+        allowNull: false,
+        primaryKey: true,
+        unique:true
+    }, 
+    cli_email: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+    cli_telefone: {
+        type: DataTypes.STRING(11),
+    },
+    cli_endereco: {
+        type: DataTypes.STRING(50),
+    },
+    cli_senha: {
+        type: DataTypes.STRING(8),
+    }
+    });
+
+
+const Chamado = database.define('Chamado', {
+    cham_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    cham_titulo: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    cham_descricao: {
+        type: DataTypes.STRING(100)
+    },
+    cham_status: {
+        type: DataTypes.ENUM('Aberto','Em andamento','Concluído'),
+        defaultValue: 'Aberto'
+    },
+    cham_data_inicio: {
+        type: DataTypes.DATEONLY,
+        defaultValue: new Date()
+    },
+    cham_prazo: {
+        type: DataTypes.VIRTUAL, // Campo virtual que não é armazenado no banco de dados
+        defaultValue: new Date(new Date().setDate(new Date().getDate() + 7))
+    }
+});
+
+// Chamado.belongsTo(Cliente, {
+//     foreignKey: "cham_cli_cpf"
+// });
+
+const Adm = database.define("Administrador", {
+    adm_nome: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+
+    adm_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+
+    adm_telefone: {
+        type: DataTypes.CHAR(11)
+    },
+
+    adm_email: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+
+    adm_senha: {
+        type: DataTypes.STRING(8)
+    }
+});
+
+const Suporte = database.define("Suporte", {
+    sup_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+
+    // sup_adm_id: {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //     references: {
+    //         model: "Administrador",
+    //         key: "adm_id"
+    //     }
+    // },
+
+    sup_nome: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+
+    sup_email: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    },
+
+    sup_telefone: {
+        type: DataTypes.STRING(11),
+    },
+
+    sup_senha: {
+        type: DataTypes.STRING(8),
+        allowNull: false
+    }
+});
+
+Suporte.belongsTo(Adm, {
+    foreignKey: "sup_adm_id"
+});
+
+const RespostaChamado = database.define("RespostaChamado", {
+    resp_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+
+    // resp_sup_id: {
+    //     type: Sequelize.INTEGER,
+    //     references: {
+    //         model: "Suporte",
+    //         key: "sup_id"
+    //     },
+    //     allowNull: false
+    // },
+
+    // resp_cham_id: {
+    //     type: Sequelize.INTEGER,
+    //     references: {
+    //         model: "Chamado",
+    //         key: "cham_id"
+    //     },
+    //     allowNull: false
+    // },
+
+    resp_data:{
+        type: DataTypes.DATEONLY,
+        defaultValue: new Date()
+    },
+
+    resp_soluc_comum: {
+        type: DataTypes.STRING(100)
+    }
+});
+
+RespostaChamado.belongsTo(Suporte, {
+    foreignKey: "resp_sup_id"
+});
+
+RespostaChamado.belongsTo(Chamado, {
+    foreignKey: "resp_cham_id"
+});
+
+const Equipamento = database.define("Equipamento", {
+    equ_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    equ_numserie: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    equ_descricao: {
+        type: DataTypes.STRING
+    }
+
+    // equ_cham_id: {
+    //     type: Sequelize.INTEGER,
+    //     references: {
+    //         model: "Chamado",
+    //         key: "cham_id"
+    //     },
+    // },
+
+    // equ_resp_id: {
+    //     type: Sequelize.INTEGER,
+    //     references:{
+    //         model: "RespostaChamado",
+    //         key: "resp_id"
+    //     }
+    // }
+});
+
+Equipamento.belongsTo(Chamado, {
+    foreignKey: "equ_cham_id"
+});
+
+Equipamento.belongsTo(RespostaChamado, {
+    foreignKey: "equ_sup_id"
+});
+
+database.sync();
+
+const modules = {database, Cliente, Chamado, Adm, Suporte, RespostaChamado, Equipamento}
+
+export default modules;
