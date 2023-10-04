edd3559d6675e34ecefa27949783f9d7d348db89
Ryan <negoryanaraujo@gmail.com>
2023-10-04 15:24:45 -0300

    Correção de identação na pagina 404

--- call-genie/src/components/404.tsx
+++ call-genie/src/components/404.tsx
@@ -1,38 +1,34 @@
-< !DOCTYPE html >
-    <html lang="pt-br">
-        <head>
-            <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <link rel="shortcut icon" href="./assets/img/Vector.svg" type="image/x-icon">
-                        <title>ERRO</title>
-                        <style>
-                            @import url(https://fonts.googleapis.com/css?family=opensans:500);
-                        </head>
-                        <body>
-                            import React from 'react';
-                            import '../Cadastro Usuários/404.css';
-                            import {any} from 'prop-types';
+import React from 'react';
+import '../Cadastro Usuários/404.css';
+import { any } from 'prop-types';
 
-                            function NotFoundPage() {
+function NotFoundPage() {
     return (
-                            <React.Fragment>
-                                <Head />
-                                <Header />
-                                <Body />
-                            </React.Fragment>
-                            )
+        <React.Fragment>
+            <Head />
+            <Header />
+            <Main />
+            <Footer />
+        </React.Fragment>
+    )
 }
 
 
-                            function Head() {
+function Head() {
     return (
-                            <head>
-                                <title>ERRO</title>
-                            </head>
-                            )
+            <head>
+                <meta charSet="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="shortcut icon" href="./assets/img/Vector.svg" type="image/x-icon" />
+                <title>ERRO</title>
+                <style>
+                    @import url(https://fonts.googleapis.com/css?family=opensans:500);
+                </style>
+            </head>
+            )
 }
 
-                            function Header() {
+                function Header() {
     return (
                             <header>
                                 <a href="/"><img src="assets/img/CallGenielogosvg.svg" className="logonome" alt="Logo - CallGenius" /></a>
@@ -47,30 +43,33 @@
                                     <div className="cloud x1"></div>
                                     <div className="cloud x1_5"></div>
                                     <div className="cloud x2"></div>
-                                    <div class="cloud x3"></div>
-                                    <div class="cloud x4"></div>
-                                    <div class="cloud x5"></div>
+                                    <div className="cloud x3"></div>
+                                    <div className="cloud x4"></div>
+                                    <div className="cloud x5"></div>
                                 </div>
-                                <div class='c'>
-                                    <div class='_404'>404</div>
-                                    <hr>
-                                        <br>
-                                            <div class='_1'>A PÁGINA</div>
-                                            <br>
-                                                <div class='_2'>NÃO FOI ENCONTRADA</div>
-                                                <br>
-                                                    <a class='btn' href='/'>VOLTAR PARA O INÍCIO</a>
-                                                </div>
-                                            </body>
-                                            )
+                                <div className='c'>
+                                    <div className='_404'>404</div>
+                                    <hr />
+                                    <br>
+                                        <div className='_1'>A PÁGINA</div>
+                                    </br>
+                                    <br>
+                                        <div className='_2'>NÃO FOI ENCONTRADA</div>
+                                    </br>
+                                    <br>
+                                        <a className='btn' href='/'>VOLTAR PARA O INÍCIO</a>
+                                    </br>
+                                </div>
+                            </body>
+                            )
 }
 
-                                            function Footer() {
+                            function Footer() {
     return (
-                                            <footer>
-                                                Copyright © 2023 Syntax Squad | Todos os direitos reservados
-                                            </footer>
-                                            )
+                            <footer>
+                                Copyright © 2023 Syntax Squad | Todos os direitos reservados
+                            </footer>
+                            )
 }
 
-                                            export default NotFoundPage;
+                            export default NotFoundPage;
