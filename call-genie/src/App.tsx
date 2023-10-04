874db2332ca757b158402481dd66cfbbdfa03a8a
Wellington Luiz de Faria <gabrieldreisilva4@gmail.com>
2023-10-04 11:32:52 -0300

    Tela opções de cadastro
    criação tela de opções de cadastro com React

--- call-genie/src/App.tsx
+++ call-genie/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import * as RoutesName from "./constants/routes";
 import Chamados from "./components/Chamados";
 import Home from "./components/Home";
+import Cadastro from "./components/cadastro";
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
                 <Route path={RoutesName.chamados} element={<Chamados />} />
                 <Route path={RoutesName.abrirChamado} />
                 <Route path={RoutesName.login} />
+                <Route path={RoutesName.cadastro} element={<Cadastro />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ call-genie/src/components/cadastro.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import '../Cadastro Usuários/cadastro.css';
+import { any } from 'prop-types';
+
+function Cadastro() {
+    return (
+        <React.Fragment>
+            <Head />
+            <Header />
+            <Main />
+            <Footer />
+        </React.Fragment>
+    )
+}
+
+
+function Head() {
+    return (
+        <head>
+            <title>Opções de cadastro</title>
+        </head>
+    )
+}
+
+function Header() {
+    return (
+        <header>
+            <a href="/"><img src="assets/img/CallGenielogosvg.svg" className="logonome" alt="Logo - CallGenius" /></a>
+        </header>
+    )
+}
+
+function Main() {
+    return (
+        <main>
+            <h2>Em qual das opções você se encaixa?</h2>
+            <div className="cadastro-opcoes">
+                <a href="/cadastrar">
+                    <div className="cadastro-opcao">
+                        <img src="assets/img/user.png" alt="SUPORTE" />
+                        <p>SOU SUPORTE</p>
+                    </div>
+                </a>
+                <a href="/cadastrar">
+                    <div className="cadastro-opcao">
+                        <img src="assets/img/user.png" alt="CLIENTE" />
+                        <p>SOU CLIENTE</p>
+                    </div>
+                </a>
+            </div>
+            <p><a href="#">Clique Aqui se precisar de ajuda</a></p>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            Copyright © 2023 Syntax Squad | Todos os direitos reservados
+        </footer>
+    )
+}
+
+export default Cadastro;
