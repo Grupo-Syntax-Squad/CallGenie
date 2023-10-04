31e2029668f6ac6d87818bead7bccb39040ba3d3
Ryan <negoryanaraujo@gmail.com>
2023-10-04 16:14:46 -0300

    Correção das rotas para a pagina de erro "404"

--- call-genie/src/App.tsx
+++ call-genie/src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import * as RoutesName from "./constants/routes";
 import Chamados from "./components/Chamados";
 import Home from "./components/Home";
 import Cadastro from "./components/cadastro";
 import Cadastrar from "./components/cadastroopcao";
+import NotFoundPage from "./components/404";
 
 export default function App() {
     return (
@@ -15,7 +16,8 @@ export default function App() {
                 <Route path={RoutesName.login} />
                 <Route path={RoutesName.cadastro} element={<Cadastro />} />
                 <Route path={RoutesName.cadastrar} element={<Cadastrar />} />
-                {/* <Route path="*" element={<NotFoundPage />} /> */}
+                <Route path= {RoutesName.notFound} element ={<NotFoundPage />} />
+                <Route path= "*" element={< Navigate to="/404" />} />
             </Routes>
         </BrowserRouter>
     );
