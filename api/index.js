2ad95f6c2b55a32d837132b84dcd6d90b71832fa
b4hia <91472871+WellingtonLFaria@users.noreply.github.com>
2023-10-06 10:47:24 -0300

    Requisições para clientes concluídas

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Chamado } from "./db.js";
+import { Chamado, Cliente } from "./db.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -52,6 +52,60 @@ app.delete("/chamados/:id", async (req, res) => {
         }
     })
     res.status(200).json({ mensagem: "Chamado deletado" })
+});
+
+app.get("/clientes", async (req, res) => {
+    let clientes = await Cliente.findAll();
+    res.json(clientes);
+});
+
+app.post("/clientes", async (req, res) => {
+    try {
+        let cliente = await Cliente.create({
+            cli_nome: req.body.nome,
+            cli_cpf: req.body.cpf,
+            cli_email: req.body.email,
+            cli_telefone: req.body.telefone,
+            cli_endereço: req.body.endereço,
+            cli_senha: req.body.senha
+        });
+        res.json(cliente);
+    } catch (error) {
+        res.json(error["original"]["sqlMessage"]);
+    };
+});
+
+app.get("/clientes/:cpf", async (req, res) => {
+    let cliente = await Cliente.findOne({
+        where: {
+            cli_cpf: req.params.cpf
+        }
+    });
+    res.json(cliente);
+});
+
+app.put("/clientes/:cpf", async (req, res) => {
+    await Cliente.update({
+        cli_nome: req.body.nome,
+        cli_email: req.body.email,
+        cli_telefone: req.body.telefone,
+        cli_endereco: req.body.endereco,
+        cli_senha: req.body.senha
+    }, {
+        where: {
+            cli_cpf: req.params.cpf
+        }
+    });
+    res.json({ mensagem: "Cliente alterado" });
+});
+
+app.delete("/clientes/:cpf", async (req, res) => {
+    await Cliente.destroy({
+        where: {
+            cli_cpf: req.params.cpf
+        }
+    })
+    res.status(200).json({ mensagem: "Cliente deletado" })
 });
 
 app.listen(80);
