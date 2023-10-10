5eb8983c6806af07a2ec0a7b2ed88a83a1c51177
b4hia <gabrieldreisilva4@gmail.com>
2023-10-10 11:59:21 -0300

    inicio correção 404

--- call-genie/src/components/404.tsx
+++ call-genie/src/components/404.tsx
@@ -1,86 +1,42 @@
 import React from "react";
-import "./style/404.css";
+import Erro404 from "./404.module.css";
 import { any } from "prop-types";
 
-function NotFoundPage() {
-  return (
-    <React.Fragment>
-      <Head />
-      <Header />
-      <Main />
-      <Footer />
-    </React.Fragment>
-  );
-}
 
-function Head() {
+export default function NotFoundPage() {
   return (
-    <head>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link
-        rel="shortcut icon"
-        href="./assets/img/Vector.svg"
-        type="image/x-icon"
-      />
-      <title>ERRO</title>
-      <style>
-        @import url(https://fonts.googleapis.com/css?family=opensans:500);
-      </style>
-    </head>
-  );
-}
-
-function Header() {
-  return (
-    <header>
-      <a href="/">
-        <img
-          src="assets/img/CallGenielogosvg.svg"
-          className="logonome"
-          alt="Logo - CallGenius"
-        />
-      </a>
-    </header>
-  );
-}
-
-function Main() {
-  return (
-    <body>
-      <div id="clouds">
-        <div className="cloud x1"></div>
-        <div className="cloud x1_5"></div>
-        <div className="cloud x2"></div>
-        <div className="cloud x3"></div>
-        <div className="cloud x4"></div>
-        <div className="cloud x5"></div>
-      </div>
-      <div className="c">
-        <div className="_404">404</div>
-        <hr />
-        <br>
-          <div className="_1">A PÁGINA</div>
-        </br>
-        <br>
-          <div className="_2">NÃO FOI ENCONTRADA</div>
-        </br>
-        <br>
-          <a className="btn" href="/">
+    <>
+      <header>
+        <a href="/">
+          <img
+            src="assets/img/CallGenielogosvg.svg"
+            className={Erro404.logonome}
+            alt="Logo - CallGenius"
+          />
+        </a>
+      </header>
+      <body>
+        <div id={Erro404.clouds}>
+          <div className={Erro404.cloud_x1}></div>
+          <div className={Erro404.cloud_x1_5}></div>
+          <div className={Erro404.cloud_x2}></div>
+          <div className={Erro404.cloud_x3}></div>
+          <div className={Erro404.cloud_x4}></div>
+          <div className={Erro404.cloud_x5}></div>
+        </div>
+        <div className={Erro404.c}>
+          <div className={Erro404._404}>404</div>
+          <hr />
+          <div className={Erro404._1}>A PÁGINA</div>
+          <div className={Erro404._2}>NÃO FOI ENCONTRADA</div>
+          <a className={Erro404.btn} href="/">
             VOLTAR PARA O INÍCIO
           </a>
-        </br>
-      </div>
-    </body>
+        </div>
+      </body>
+      <footer>
+        Copyright © 2023 Syntax Squad | Todos os direitos reservados
+      </footer>
+    </>
   );
 }
-
-function Footer() {
-  return (
-    <footer>
-      Copyright © 2023 Syntax Squad | Todos os direitos reservados
-    </footer>
-  );
-}
-
-export default NotFoundPage;
