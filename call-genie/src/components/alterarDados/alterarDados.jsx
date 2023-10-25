71090ebcf1ebc14daccdcd834082371aff2974f6
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-10-25 11:39:12 -0300

    Concluídas as funções de alterar dados

--- call-genie/src/components/alterarDados/alterarDados.jsx
+++ call-genie/src/components/alterarDados/alterarDados.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Alterardados from "./alterarDados.module.css";
 import axios from "axios"
 
@@ -9,98 +9,101 @@ function handleChange(event) {
 };
 
 function handleSubmit(event) {
-    axios.get(`http://localhost:8080/clientes/${localStorage.getItem("login")}`).then(response => {
-      console.log(response);
-      console.log(localStorage.getItem('login'))
+  axios.get(`http://localhost:8080/clientes/${localStorage.getItem("login")}`).then(response => {
+    if (infos.inputSenhaAtual != response.data.cli_senha) {
+      alert("Senha atual incorreta!");
+    } else {
       axios.put(`http://localhost:8080/clientes/${localStorage.getItem("login")}`, {
         nome: response.data.cli_nome,
-        email: infos.inputNovoEmail,
-        telefone: infos.inputNovoTelefone,
-        endereco: infos.inputNovoCep,
-        senha: infos.inputNovaSenha
+        email: infos.inputNovoEmail == "" ? response.data.cli_email : infos.inputNovoEmail,
+        telefone: infos.inputNovoTelefone == "" ? response.data.cli_telefone : infos.inputNovoTelefone,
+        endereco: infos.inputNovoCep == "" ? response.data.cli_endereco : infos.inputNovoCep,
+        senha: infos.inputNovaSenha == "" ? response.data.cli_senha : infos.inputNovaSenha
       });
-    })
+      alert("Informaçõs alteradas com sucesso!");
+    };
+  });
   event.preventDefault();
 };
 
 export default function AlterarDados() {
 
   const [inf, setInf] = useState({})
-  useEffect (() => {axios.get(`http://localhost:8080/clientes/${localStorage.getItem('login')}`).then(response => {setInf(response.data)})});
+  useEffect(() => { axios.get(`http://localhost:8080/clientes/${localStorage.getItem('login')}`).then(response => { setInf(response.data) }) });
 
   return (
     <>
       <header className={Alterardados.header}>
-      <a href="/">
-      <img
-        src="assets/img/CallGenielogosvg.svg"
-        alt="Logo"
-        id={Alterardados.logoIndexAlterarDados}
-      />
-    </a>
+        <a href="/">
+          <img
+            src="assets/img/CallGenielogosvg.svg"
+            alt="Logo"
+            id={Alterardados.logoIndexAlterarDados}
+          />
+        </a>
       </header>
-          <div className={Alterardados.main_div}>
-          <div className={Alterardados.flexRowAlterarDados}>
-            <img
-                  src="assets/img/iconback.png"
-                  alt="Voltar"
-                  id={Alterardados.iconback}
-                />
-              <h2 id={Alterardados.h2_AlterarDados}>Alterar Dados de Cadastro</h2>
-          </div>
-            <div className={Alterardados.fundoCadastro}>
-      <main>
-              <form action="" className={Alterardados.alterarDados_container} onSubmit={handleSubmit} >
-                <label>
-                  <input
-                    type="text"
-                    id="inputNovoEmail"
-                    placeholder={inf.cli_email}
-                    onChange={handleChange}
-                  />
-                </label>
-                <label>
-                  <input
-                    type="text"
-                    id="inputNovoTelefone"
-                    placeholder={inf.cli_telefone}
-                    onChange={handleChange}
-                  />
-                </label>
-                <label>
-                  <input
-                    type="text"
-                    id="inputNovoCep"
-                    placeholder={inf.cli_endereco}
-                    onChange={handleChange}
-                  />
-                </label>
-                <label>
-                  <input
-                    type="text"
-                    id="inputNovaSenha"
-                    placeholder="Nova senha (deixe este campo em branco caso não queira alterá-lo)"
-                    onChange={handleChange}
-                  />
-                </label>
-                <label>
-                  <input
-                    type="text"
-                    id="inputSenhaAtual"
-                    placeholder="Senha atual*"
-                    // required
-                    // onChange={handleChange}
-                  />
-                </label>
+      <div className={Alterardados.main_div}>
+        <div className={Alterardados.flexRowAlterarDados}>
+          <img
+            src="assets/img/iconback.png"
+            alt="Voltar"
+            id={Alterardados.iconback}
+          />
+          <h2 id={Alterardados.h2_AlterarDados}>Alterar Dados de Cadastro</h2>
+        </div>
+        <div className={Alterardados.fundoCadastro}>
+          <main>
+            <form action="" className={Alterardados.alterarDados_container} onSubmit={handleSubmit} >
+              <label>
                 <input
-                  type="submit"
-                  value="Salvar Alterações"
-                  className={Alterardados.buttonInput}
+                  type="text"
+                  name="inputNovoEmail"
+                  placeholder={inf.cli_email}
+                  onChange={handleChange}
                 />
-              </form>
-      </main>
-            </div>
-          </div>
+              </label>
+              <label>
+                <input
+                  type="text"
+                  name="inputNovoTelefone"
+                  placeholder={inf.cli_telefone}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                <input
+                  type="text"
+                  name="inputNovoCep"
+                  placeholder={inf.cli_endereco}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                <input
+                  type="text"
+                  name="inputNovaSenha"
+                  placeholder="Nova senha (deixe este campo em branco caso não queira alterá-lo)"
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                <input
+                  type="text"
+                  name="inputSenhaAtual"
+                  placeholder="Senha atual*"
+                required
+                onChange={handleChange}
+                />
+              </label>
+              <input
+                type="submit"
+                value="Salvar Alterações"
+                className={Alterardados.buttonInput}
+              />
+            </form>
+          </main>
+        </div>
+      </div>
       <footer>
         Copyright © 2023 Syntax Squad | Todos os direitos reservados
       </footer>
