f0d1bee521b8defda9411273c4e8bed7f19154a8
gabriel santos <gabriel.santos157@fatec.sp.gov.br>
2023-10-31 08:16:00 -0300

    colocando filtro na pagina de abrir chamados

--- call-genie/src/App.jsx
+++ call-genie/src/App.jsx
@@ -45,7 +45,7 @@ export default function App() {
                 <Route path={RoutesName.contato} element={<Contato />} />
                 <Route path={RoutesName.entrar} element={<Entrar />} />
                 <Route path={RoutesName.cliente} element={<Cliente />} />
-                <Route path={RoutesName.chamadosSuporte} element={<Chamados />} />
+                <Route path={RoutesName.chamadosSuporte} element={<ChamadosSuporte />} />
                 <Route path={RoutesName.admin} element={localStorage.getItem("login") == "" ? <DoLogin /> : <Admin />} />
                 <Route path={RoutesName.cadastrarFAQ} element={<CadastrarFAQ />} />
                 <Route path={RoutesName.relatorios}element={localStorage.getItem("login") == "" ? <DoLogin /> : <Relatorios />} />
