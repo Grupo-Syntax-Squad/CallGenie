2d9def45587a1b140a56cf0cd50f34bb10f4d00b
João Vitor Pereira <joao.joaovitorp481@gmail.com>
2023-10-31 22:45:21 -0300

    Finalizando estilização do relatório

--- call-genie/src/components/relatorios/relatorio.tsx
+++ call-genie/src/components/relatorios/relatorio.tsx
@@ -3,16 +3,16 @@ import RelatorioPag from "./relatorioPag.module.css";
 
 export default function Relatorio() {
   return (
-    <div>
-      <div className={RelatorioPag.flex}>
+    <div className={RelatorioPag.text_black}>
+      <div className={RelatorioPag.flexCabecalho}>
         <img
           src="assets/img/CallGenie_Black.png"
           alt="Logo"
           id={RelatorioPag.logoindex}
         />
-        <p>Id do Chamado: </p>
+        <h2 className={RelatorioPag.marginBottom}><strong>Id do Chamado: xxxxx</strong></h2>
       </div>
-      <p>Chamado respondido por: nome do suporte</p>
+      <p className={RelatorioPag.text_left}>Chamado respondido por: nome do suporte</p>
       <div className={RelatorioPag.center}>
         <div className={RelatorioPag.flex}>
           <p>Nome do cliente</p>
@@ -22,39 +22,45 @@ export default function Relatorio() {
       </div>
       <div className={RelatorioPag.divisor}></div>
       <div className={RelatorioPag.flex}>
-          <h2>TÍtulo do chamado</h2>
+          <h3>TÍtulo do chamado</h3>
           <h3>prioridade do chamado</h3>
-          <p>criado em: xx/xx/xxxx</p>
+          <h3>criado em: xx/xx/xxxx</h3>
       </div>
-      <p className={RelatorioPag.flex}>
-        <span>Descrição:</span>Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Reprehenderit consequatur nulla rem placeat incidunt
-        labore accusamus, eum, dicta expedita nihil iusto explicabo architecto,
-        enim recusandae aperiam! Provident corrupti minima ullam.
+      <p className={RelatorioPag.text_justify}>
+        <div className={RelatorioPag.marginBottom}>
+          <strong>Descrição:</strong>Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Reprehenderit consequatur nulla rem placeat incidunt
+          labore accusamus, eum, dicta expedita nihil iusto explicabo architecto,
+          enim recusandae aperiam! Provident corrupti minima ullam.
+        </div>
       </p>
-      <p className={RelatorioPag.flex}>
-        <span>Equipamento:</span>Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Iste sed nesciunt totam cum odit vero, perspiciatis
-        accusantium dolores nulla quam quia fugiat excepturi deleniti magni
-        doloremque laborum officia, debitis ut!
+      <p className={RelatorioPag.text_justify}>
+        <div className={RelatorioPag.marginBottom}>
+          <strong>Equipamento:</strong>Nome  ||  Número de Série  ||  Tipo de Equipamento
+        </div>
+
       </p>
-      <p className={RelatorioPag.flex}>
-        <span>Resposta:</span>Lorem ipsum dolor, sit amet consectetur
+      <p className={RelatorioPag.text_justify}>
+        <strong>Resposta:</strong>Lorem ipsum dolor, sit amet consectetur
         adipisicing elit. Expedita id, officia ut possimus adipisci rem repellat
         animi quo, laudantium sunt consequuntur? Tenetur animi temporibus
         aliquid doloremque veniam minus voluptatum quae.
       </p>
-      <p className={RelatorioPag.flex}>
-        <span>Comentário:</span>Lorem, ipsum dolor sit amet consectetur
+      <p className={RelatorioPag.text_right}><strong className={RelatorioPag.small_text}>Respondido em: xx/xx/xxxx</strong></p>
+      <p className={RelatorioPag.text_justify}>
+        <strong>Comentário:</strong>Lorem, ipsum dolor sit amet consectetur
         adipisicing elit. Cumque commodi nulla dolore! Ut quae veritatis
         perspiciatis quo labore assumenda nostrum vel blanditiis earum laborum!
         Perspiciatis beatae dolorum quibusdam eveniet suscipit!
       </p>
-      <p className={RelatorioPag.flex}>Encerrado em: xx/xx/xxxx</p>
-      <footer>
-        <p>Copyright © 2023 Syntax Squad | Todos os direitos reservados</p>
-        <p>Relatório gerado em: xx/xx/xxxx</p>
-      </footer>
+      <p className={RelatorioPag.text_right}><div className={RelatorioPag.marginBottom}><strong className={RelatorioPag.small_text}>Comentário adicionado em: xx/xx/xxxx</strong></div></p>
+      <p className={RelatorioPag.flex}><strong>Encerrado em: xx/xx/xxxx</strong></p>
+      <div className={RelatorioPag.footerRelatorio}>
+        <footer className={RelatorioPag.flex}>
+          <p className={RelatorioPag.small_text}>Copyright © 2023 Syntax Squad | Todos os direitos reservados</p>
+          <p className={RelatorioPag.marginLeft}><strong>Relatório gerado em: xx/xx/xxxx</strong></p>
+        </footer>
+      </div>
     </div>
   );
 }
