49cde2ef8f2f2ccda8eadf84463a5c04b0ea2355
gabriel santos <gabriel.santos157@fatec.sp.gov.br>
2023-11-23 10:49:30 -0300

    add barra de pesquisa por ID

--- call-genie/src/components/Chamados/Chamados.jsx
+++ call-genie/src/components/Chamados/Chamados.jsx
@@ -1,144 +1,9 @@
-// import React, { useEffect, useState } from "react";
-// import ChamadosPageCSS from "./ChamadosPage.module.css";
-// import HeaderChamado from '../HeaderChamado/headerChamado.module.css'
-// import StyleTableCSS from './StyleTable.module.css'
-// import axios from "axios";
-
-// function handleSubmit(event) {
-//   let id = event.target.name;
-//   axios.delete(`http://localhost:8080/chamados/${id}`);
-//   window.location.replace("/chamados");
-//   event.preventDefault();
-// };
-
-// function chamadoPage(e) {
-//   console.log(e.target.id);
-//   localStorage.setItem("cham_id", e.target.id);
-//   window.location.replace("/chamadoAberto");
-// };
-
-
-// function Table({ chamadoPage }) {
-//   const [chamados, setChamados] = useState([])
-//   // const [modal, setModal] = useState(false)
-
-//   let cpf = localStorage.getItem("login");
-//   useEffect(() => {
-//     if (cpf.length === 1) {
-//       axios.get(`http://localhost:8080/chamados`).then(response => setChamados(response.data));
-//     } else {
-//       axios.get(`http://localhost:8080/chamados/cpf/${cpf}`).then(response => setChamados(response.data));
-//     };
-//   });
-
-//   let listaChamados = chamados.map(chamado =>
-//     <tr>
-//       <td id={chamado.cham_id} onClick={chamadoPage} style={{cursor: "pointer", color: "blue", textDecoration: "underline"}}>{chamado.cham_titulo}</td>
-//       <td>{chamado.cham_id}</td>
-//       <td>{new Date(new Date().setDate(new Date(chamado.cham_data_inicio).getDate() + 1)).toLocaleDateString()}</td>
-//       <td>{chamado.cham_status}</td>
-//       {cpf.length == 1 ? <></> : <td><button id="botaoDeletarChamado" type="submit">Deletar</button></td>}
-//     </tr>
-//   );
-
-//   return (
-//     <table>
-//       <thead>
-//         <tr className={StyleTableCSS.ptable}>
-//           <th><p>Título</p></th>
-//           <th><p>ID </p></th>
-//           <th><p>Data de criação</p> </th>
-//           <th><p>Status </p></th>
-//         </tr>
-//       </thead>
-//       <tbody>{listaChamados}</tbody>
-//     </table>
-//   );
-// };
-
-// export default function Chamados() {
-//   let cpf = localStorage.getItem("login");
-
-//   return (
-//     <body className={ChamadosPageCSS.Body}>
-//       <div className={ChamadosPageCSS.bodyChamados}>
-//         <header>
-//           <a href="/">
-//             <img
-//               src="assets/img/Vector.svg"
-//               className={HeaderChamado.logogenie}
-//               alt="Logo - CallGenius"
-//             />
-//           </a>
-//           <div className={HeaderChamado.headerItensRight}>
-//             <a href="/alterarDados">
-//               <img
-//                 src="assets/img/iconeuser2.png"
-//                 alt="Usuário"
-//               />
-//             </a>
-//             <h2>Olá, {cpf.length == 1 ? "Suporte" : "User"}</h2>
-//             <a href="/entrar">
-//               <img
-//                 src="assets/img/iconexit.png"
-//                 alt="Sair"
-//               />
-//             </a>
-//           </div>
-//         </header>
-//         <div className={ChamadosPageCSS.ajuda}>
-//           <a href="/FAQ">Preciso de ajuda?</a>
-//         </div>
-
-//         <main className={ChamadosPageCSS.fundoChamado}>
-//           <div className={HeaderChamado.fundo}>
-//             <div className={ChamadosPageCSS.titulo}>
-//               <h2>Meus Chamados</h2>
-//             </div>
-//             <div className={ChamadosPageCSS.filtrosContainer}>
-//               <div className={ChamadosPageCSS.filtros}>
-//                 <p>Meus Chamados  <i className="fa-solid fa-arrow-down"></i></p>
-//               </div>
-//               <div className={ChamadosPageCSS.filtros}>
-//                 <p>Buscar por título <i className="fa-solid fa-magnifying-glass"></i></p>
-//               </div>
-//             </div>
-//             <div className={StyleTableCSS.mainTable}>
-//               <section className={StyleTableCSS.tableBody}>
-//                 <Table chamadoPage={chamadoPage} />
-//               </section>
-//               <dialog open>
-//                 <p>
-//                   VOCÊ ESTÁ PRESTES A DELETAR UMA ORDEM DE SERVIÇO, DESEJA
-//                   CONFIRMAR?
-//                 </p>
-//                 <form method="post" onSubmit={handleSubmit}><button className={ChamadosPageCSS.sim}>SIM</button></form>
-//                 <button className={ChamadosPageCSS.nao}>NÃO</button>
-//               </dialog>
-//             </div>
-//             <div className={ChamadosPageCSS.buttoncontainer}>
-//             {cpf.length == 1 ? <></> : <a href="/abrirChamado">
-//               <button className={ChamadosPageCSS.cadastrobutton}>Abrir Chamado</button>
-//             </a>}
-//             <a href="/relatorios">
-//               <button className={ChamadosPageCSS.cadastrobutton}>Relatórios</button>
-//             </a>
-//           </div>
-//           </div>
-//         </main>
-//       </div>
-//       <footer>
-//         Copyright © 2023 Syntax Squad | Todos os direitos reservados
-//       </footer>
-//     </body>
-//   );
-// };
-
 import React, { useEffect, useState } from "react";
 import ChamadosPageCSS from "./ChamadosPage.module.css";
 import HeaderChamado from '../HeaderChamado/headerChamado.module.css'
 import StyleTableCSS from './StyleTable.module.css'
 import axios from "axios";
+import { version } from "react-dom";
 
 function handleDeleteChamados(selectedChamados) {
   if (selectedChamados.length === 0) {
@@ -158,7 +23,7 @@ function chamadoPage(e) {
   window.location.replace("/chamadoAberto");
 };
 
-function Table({ selectedChamados, handleCheckboxChange, chamadoPage }) {
+function Table({ selectedChamados, handleCheckboxChange, chamadoPage, filtro }) {
   const [chamados, setChamados] = useState([]);
   let cpf = localStorage.getItem("login");
 
@@ -170,7 +35,18 @@ function Table({ selectedChamados, handleCheckboxChange, chamadoPage }) {
     };
   });
 
-  const listaChamados = chamados.map((chamado) => (
+  let cham = [];
+  console.log(typeof filtro)
+
+  if (filtro === "") {
+    cham = chamados;
+  } else {
+    chamados.forEach(chamado => {
+      if (chamado.cham_id == filtro) cham.push(chamado)
+    });
+  };
+
+  const listaChamados = cham.map((chamado) => (
     <tr key={chamado.cham_id}>
       <td
         id={chamado.cham_id}
@@ -215,13 +91,23 @@ export default function Chamados() {
 
   const userCpf = localStorage.getItem("login");
 
+  const [chamados, setChamados] = useState([]);
+
+  useEffect(() => {
+    if (cpf.length === 1) {
+      axios.get(`http://localhost:8080/chamados`).then(response => setChamados(response.data));
+    } else {
+      axios.get(`http://localhost:8080/chamados/cpf/${cpf}`).then(response => setChamados(response.data));
+    };
+  });
+
   useEffect(() => {
 
     const isSupport = userCpf.length === 1;
     const fetchUsername = async () => {
       try {
         const response = await axios.get(
-            `http://localhost:8080/${isSupport ? "suportes" : "clientes"}/${userCpf}`
+          `http://localhost:8080/${isSupport ? "suportes" : "clientes"}/${userCpf}`
         );
         const name = isSupport ? response.data.sup_nome : response.data.cli_nome;
         setUserName(name);
@@ -240,6 +126,10 @@ export default function Chamados() {
       setSelectedChamados([...selectedChamados, chamadoId]);
     }
   };
+
+  const [searchValue, setSearchValue] = useState("");
+
+
 
   return (
     <body className={ChamadosPageCSS.Body}>
@@ -279,15 +169,20 @@ export default function Chamados() {
             </div>
             <div className={ChamadosPageCSS.filtrosContainer}>
               <div className={ChamadosPageCSS.filtros}>
-                <p>Meus Chamados  <i className="fa-solid fa-arrow-down"></i></p>
+                <button>Meus Chamados  <i className="fa-solid fa-arrow-down"></i></button>
               </div>
-              <div className={ChamadosPageCSS.filtros}>
-                <p>Buscar por título <i className="fa-solid fa-magnifying-glass"></i></p>
+              <div className={ChamadosPageCSS.filtro}>
+                <input
+                  type="number"
+                  placeholder="escreva o ID que você quer procurar aqui."
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
               </div>
             </div>
             <div className={StyleTableCSS.mainTable}>
               <section className={StyleTableCSS.tableBody}>
-                <Table selectedChamados={selectedChamados} handleCheckboxChange={handleCheckboxChange} chamadoPage={chamadoPage} />
+                <Table selectedChamados={selectedChamados} handleCheckboxChange={handleCheckboxChange} chamadoPage={chamadoPage} filtro={searchValue} />
               </section>
               {showDeleteDialog && (
                 <dialog open>
