c6b62a1926f7e6fe0feba08b722eb0f06e0c2ada
WellingtonLFaria <wellingtonll.faria@gmail.com>
2023-11-27 10:41:30 -0300

    Removendo call-genie

--- call-genie/src/components/DoLogin.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function DoLogin() {
-    return (
-        <>
-        <h1>Faça <a href="/entrar">Login</a> para acessar a página de chamados</h1>
-        </>
-    );
-};
